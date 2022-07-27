47cf214837b68831b56d5e9b1985a9da16d6e316
hyebun <hyebun96@naver.com>
2022-07-27 21:41:35 +0900

    #21 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트 까지 한번에 적용

--- project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -34,6 +34,7 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
+    //@Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticleView() throws Exception {
@@ -44,7 +45,7 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
-                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
     }
 
